a243fc1e0b9f753a7adbf2285bfdeb413a086aa7
AlicePeng127775 <yuan127775@gmail.com>
2024-08-12 15:04:10 -0700

    update

---
+++ project5.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Explore Alice Peng's portfolio featuring web development projects including the Whac-A-Mole Website GameAnimal Adoption Website. Discover innovative solutions in HTML, CSS, JavaScript, UI/UX design, and more.">
+    <meta name="keywords" content="Alice Peng, Web Development, UI/UX Design, HTML, CSS, JavaScript, Visual Studio Code, WordPress, React, Portfolio">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="dist/app.css">
+    <link rel="icon" type="image/x-icon" href="/img/logo/icon.png">
+    <script src="https://kit.fontawesome.com/92a4d8c871.js" crossorigin="anonymous"></script>
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bodymovin/5.9.6/lottie.min.js"></script>
+
+    <!-- Open Graph Meta Tags -->
+        <meta property="og:title" content="Alice Peng - Narra Furniture App">
+        <meta property="og:description" content="Hello! I'm Alice Peng, I designed and developed the App and website for Narra Furniture.">
+        <meta property="og:image" content="https://alicepeng.com/img/project/project5.png">
+        <meta property="og:url" content="https://alicepeng.com/project5">
+        <meta property="og:type" content="website">
+        <meta property="fb:app_id" content="yuan.zi.3133">
+    <!-- Twitter Card Meta Tags -->
+        <meta name="twitter:card" content="summary_large_image">
+        <meta name="twitter:title" content="Alice Peng - Narra Furniture App">
+        <meta name="twitter:description" content="Hello! I'm Alice Peng, I designed and developed the App and website for Narra Furniture.">
+        <meta name="twitter:image" content="https://alicpeng.com/img/project/project5.png">
+        <meta name="twitter:url" content="https://alicepeng.com/project5">
+        <meta name="twitter:creator" content="@775_Yu">
+    <title> Alice's Project | Narra Furniture App </title>
+</head>
+<body>
+    <!----------------- header ------------------>
+    <?php require_once("partials/header.php") ?>
+    <!------------------------------------------->
+
+    <main class='container'>
+    <div class='portfolioTitle'>
+        <p>Explore my latest portfolio of web development projects<br>
+         covering a wide range of areas from HTML , CSS, JavaScript,<br>
+          Visual Studio Code, UI/UX animation to WordPress and React.</p>
+          <div id="lottie-animation"></div>
+
+    </div>
+    <div class='singleProject'>
+        <div class='project grid'>
+            <div class='projectInfo col-12' data-aos="fade-up">                
+                    <div class='projectIntro col-12'>
+                        <div class='project5C col-12 grid'>                        
+
+                            <div class='projectIMG col-12 col-6-lg'><img src='./img/project/DevicesMockup5.png' alt=' Narra Furniture App laptop Mockup'></div>
+                        
+                            <div class='col-12 col-6-lg'>
+                                <h4 class='col-12 col-8-lg '>Narra Furniture App</h4> 
+                                <div class='toolC'>
+                                    <span class='tool toolBorder5'><img src='./img/skill/coding/HTML.png' alt='HTML Logo'/></span>
+                                    <span class='tool toolBorder5'><img src='./img/skill/coding/CSS.png' alt='CSS Logo'/></span>
+                                    <span class='tool toolBorder5'><img src='./img/skill/coding/React.js.png' alt='React Logo'/></span>
+                                    <span class='tool toolBorder5'><img src='./img/skill/coding/Expo.png' alt='Expo Logo'/></span>
+                                    <span class='tool toolBorder5'><img src='./img/skill/coding/Visual Studio Code.png' alt='Visual Studio Code Logo'/></span>
+                                    <span class='tool toolBorder5'><img src='./img/skill/coding/Android Studio.png' alt='Android Studio Logo'/></span>
+                                </div>
+                                
+                                <h6>Narra is a furniture company in Vancouver. <br> 
+
+                                For this project, It's a Team project , we needed to design an online shopping app for Narra Furniture, <br> 
+                                from designing the branding style to developing the one-page website for downloading the app, 
+                                email templates, and develop the app <br>
+                                </h6>
+
+                                <div class='linkBtn col-12 col-4-lg'>
+                                    <a href="http://narraapp.alicepeng.com/"><i class="fa-solid fa-link"></i>Lauch&nbsp;Site</a>
+                                    <a href="#"><i class="fa-brands fa-github"></i>View&nbsp;Code</a>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div class='mockupC col-12 grid'>
+                            <div class='mockup col-12'><img src='./img/project/Mockup5.png' alt='Narra Furniture App Mockup'></div>
+                        </div>
+                        <div class='process col-12 grid'>
+                               
+                                <div class="col-12">
+
+                                    <div style="position: relative; width: 100%; height: 0; padding-top: 56.2500%;
+                                    padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden;
+                                    border-radius: 8px; will-change: transform;">
+                                    <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;"
+                                        src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAF2P1uyUxE&#x2F;9wWYo6rfwyUckXOxMlnJlw&#x2F;view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
+                                    </iframe>
+                                    </div>                                
+                                </div>
+
+
+                        </div>
+                          
+
+                        <div class='process col-12 grid'>
+
+                        
+                           
+                            <div class='col-12 col-8-lg'>
+                                <h5>App Development - Learning:</h5>
+
+                                <p>
+                                I chose to use React Native near-native to develop my app, because I'm more familiar with JavaScript and I can get used to it faster, and also because it's able to develop for both ios and Android, which saves me time and cost.<br>
+                                But since it's a new language, in addition to what I learned in class, I also searched for tutorials on youtube to help me learn React Native before I developed it. And according to the app wireframes, focus on learning the functions that may be used.                                
+                                </p>
+                                </br>
+
+                                <h5>Development:</h5>
+                                <p>
+                                I also installed expo and android Studio to view the app before development.<br>
+                                React Native provides a large number of open source plugins and libraries, 
+                                the most basic of which uses @react-native-safe-area-context to set the safe 
+                                area to ensure that the app's ui elements will not be blocked. In addition, 
+                                @react-navigation/nativ and @react-navigation/native-stack @react- native-screens 
+                                are used to complete the navigation of each page or to connect the api, 
+                                and to make the app and support better transition animation Switching pages.<br>
+                                In addition, install @rneui/themed @rneui/base in order to use React Navite Elements, 
+                                so that the ui elements can be standardized as a theme, which is more convenient 
+                                for development and post- production adjustments and changes. 
+                                For example, buttons, check boxes, Text, color palettes, etc. in the app.<br>
+
+                                Then, since React Navite Elements doesn't have a dropdown menu, we also use @react-native- dropdown-picker to fulfill the need of dropdown menu in the design.<br>
+                                Finally. To complete the design, I needed to use merriweather in google font, <br>
+                                I installed expo-font , and expo-google-fonts/merriweather, added it to the theme, 
+                                used the fonts, and added the Narra logo to React Native's ActivityIndicator during 
+                                the process of loading the app and the fonts. users can have a better experience!                         
+                                </p>
+                            </div>
+                            <div class="col-12 col-4-lg ">
+
+                                <div class='demo'>
+                                    <iframe src="https://appetize.io/embed/b_2r2l3nhbiepdzqlu74brmsz7ui" width="378px" height="800px" frameborder="0" scrolling="no"></iframe>
+                                </div>
+                            
+                            </div>
+
+                        </div>
+                        
+                       
+                        </div>
+
+                    </div>
+            </div>
+
+        </div>
+        <h2 class='other'>Other Project</h2>
+        <div class='ontherProject container grid' data-aos="fade-up">
+           
+                <div class='project col-12 col-6-sm col-6-md col-4-lg'>
+                    <div class='projectImg'><img src='./img/project/project2.png'  alt="Lumina online shopping Website"></div>
+                    <div class='projectInfo'>
+                        <h4>Lumina online shopping Website</h4>
+                        <a href='project2.php' class='viewBtn btn2'> View  <i class="fa-solid fa-arrow-right"></i> </a>
+                    </div>
+                </div>
+
+                <div class='project col-12 col-6-sm col-6-md col-4-lg'>
+                    <div class='projectImg'><img src='./img/project/project3.png' alt="Whac-A-Mole Website Game"></div>
+                    <div class='projectInfo'>
+                        <h4>Whac-A-Mole Website Game</h4>
+                        <a href='project3.php' class='viewBtn btn2'> View  <i class="fa-solid fa-arrow-right"></i> </a>
+                    </div>
+                </div>
+
+                <div class='project col-12 col-6-sm col-6-md col-4-lg'>
+                    <div class='projectImg'><img src='./img/project/project4.png' alt="October shopping app design"></div>
+                    <div class='projectInfo'>
+                        <h4>October shopping app design</h4>
+                        <a href='project4.php' class='viewBtn btn2'> View  <i class="fa-solid fa-arrow-right"></i> </a>
+                    </div>
+                </div>
+        </div>
+    </div>
+
+    </main>
+    <div class='content-wrap'></div>
+    <!----------------- footer ------------------>
+    <?php require_once("partials/footer.php") ?>
+    <!------------------------------------------->
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script src='./js/app.js'></script>
+    <script>
+        var animation = lottie.loadAnimation({
+        container: document.getElementById('lottie-animation'), // 容器 ID
+        renderer: 'svg', // 渲染器类型，可选 'svg' 或 'canvas'
+        loop: true, // 是否循环播放
+        autoplay: true, // 是否自动播放
+        path: 'https://lottie.host/50b088fe-b6b6-47b4-88f6-5d532aea5086/2a3vEVbGKX.json' // Lottie 文件的路径
+    });
+
+    var animation2 = lottie.loadAnimation({
+            container: document.getElementById('lottie-animation2'), // 容器 ID
+            renderer: 'svg', // 渲染器类型，可选 'svg' 或 'canvas'
+            loop: true, // 是否循环播放
+            autoplay: true, // 是否自动播放
+            path: 'https://lottie.host/62ecd630-5863-4f22-94ba-e80968fa0bb1/zCWiMOR3fs.json' // Lottie 文件的路径
+        });
+    </script>
+
+</body>
+</html>
---
+++ project6.php
@@ -0,0 +1,229 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Explore Alice Peng's portfolio featuring web development projects including the Whac-A-Mole Website GameAnimal Adoption Website. Discover innovative solutions in HTML, CSS, JavaScript, UI/UX design, and more.">
+    <meta name="keywords" content="Alice Peng, Web Development, UI/UX Design, HTML, CSS, JavaScript, Visual Studio Code, WordPress, React, Portfolio">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="dist/app.css">
+    <link rel="icon" type="image/x-icon" href="/img/logo/icon.png">
+    <script src="https://kit.fontawesome.com/92a4d8c871.js" crossorigin="anonymous"></script>
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bodymovin/5.9.6/lottie.min.js"></script>
+
+    <!-- Open Graph Meta Tags -->
+        <meta property="og:title" content="Alice Peng - Pokedex App">
+        <meta property="og:description" content="Hello! I'm Alice Peng, I designed and developed the website for Little Fish Cares .">
+        <meta property="og:image" content="https://alicepeng.com/img/project/project1.png">
+        <meta property="og:url" content="https://alicepeng.com/project1">
+        <meta property="og:type" content="website">
+        <meta property="fb:app_id" content="yuan.zi.3133">
+    <!-- Twitter Card Meta Tags -->
+        <meta name="twitter:card" content="summary_large_image">
+        <meta name="twitter:title" content="Alice Peng - Pokedex App">
+        <meta name="twitter:description" content="Hello! I'm Alice Peng, I designed and developed the website for Little Fish Cares.">
+        <meta name="twitter:image" content="https://alicpeng.com/img/project/project1.png">
+        <meta name="twitter:url" content="https://alicepeng.com/project1">
+        <meta name="twitter:creator" content="@775_Yu">
+    <title> Alice's Project | Pokedex App</title>
+</head>
+<body>
+    <!----------------- header ------------------>
+    <?php require_once("partials/header.php") ?>
+    <!------------------------------------------->
+
+    <main class='container'>
+    <div class='portfolioTitle'>
+        <p>Explore my latest portfolio of web development projects<br>
+         covering a wide range of areas from HTML , CSS, JavaScript,<br>
+          Visual Studio Code, UI/UX animation to WordPress and React.</p>
+          <div id="lottie-animation"></div>
+
+    </div>
+    <div class='singleProject'>
+        <div class='project grid'>
+            <div class='projectInfo col-12' data-aos="fade-up">                
+                    <div class='projectIntro col-12'>
+                        <div class='project6C col-12 grid'>                        
+
+                            <div class='projectIMG col-12 col-6-lg'><img src='./img/project/Moblio06.png' alt='Pokedex App Mockup'></div>
+                        
+                            <div class='col-12 col-6-lg'>
+                                <h4 class='col-12 col-8-lg '>Pokedex App</h4> 
+                                <div class='toolC'>
+                                    <span class='tool toolBorder6'><img src='./img/skill/coding/React.js.png' alt='React Logo'/></span>
+                                    <span class='tool toolBorder6'><img src='./img/skill/coding/Expo.png' alt='Expo Logo'/></span>
+                                    <span class='tool toolBorder6'><img src='./img/skill/coding/Visual Studio Code.png' alt='Visual Studio Code Logo'/></span>
+                                    <span class='tool toolBorder6'><img src='./img/skill/coding/Android Studio.png' alt='Android Studio Logo'/></span>
+                                </div>
+                                
+                                <h6>Poke API <br> 
+
+                                Fetch Poke API and display data on the App<br>
+                                Users can use the app to browse all Pokémon information, search for specific Pokémon by name or id,
+                                 and browse more Pokémon details.
+
+                                </h6>
+
+                                <div class='linkBtn col-12 col-4-lg'>
+                                    <a href="https://littlefishcares.aliccccc.com/"><i class="fa-solid fa-link"></i>Lauch&nbsp;Site</a>
+                                    <a href="#"><i class="fa-brands fa-github"></i>View&nbsp;Code</a>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div class='mockupC col-12 grid'>
+                            <div class='mockup col-12'><img src='./img/project/Mockup6.png' alt='Pokedex App Mockup'></div>
+                        </div>
+                        
+                          
+
+                        <div class='process col-12 grid'>
+                            <div class='col-12 col-6-lg'>
+                                <h5>Brainstorm - </br>Information Architecture:</h5>
+
+                                <p>
+                                Before designing a web page for a company, it is important to understand the company's main services, goals, missions, analyze the target audience, hypothetical scenarios, analyze the competitors.
+                                </p>
+                                </br>
+
+                                <h5>Design - Style Guide:</h5>
+                                <p>
+                                After completing the Brainstorm information architecture, before developing the website. We need to design a Style Guide for the company, which includes designing the logo, colors, fonts, buttons, wireframes, and mockups.
+                                </p>
+                            </div>
+                            <div class="col-12 col-6-lg">
+                            <div style="position: relative; width: 100%; height: 0; padding-top: 56.2500%;
+                            padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden;
+                            border-radius: 8px; will-change: transform;">
+                            <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;"
+                                src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAFoS6Q87Uc&#x2F;iIO3nkjqEx8AHjNPwP2H9w&#x2F;view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
+                            </iframe>
+                            </div>
+
+                            </div>
+
+                        </div>
+                        
+                        <div class='process col-12 grid'>
+                            <div class="col-12 col-6-lg">
+                            <iframe style="border: 1px solid rgba(0, 0, 0, 0.1);" width="100%" height="450" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fdesign%2FUX5J2rhHJYjz5tuDLmsFdT%2FLittle-Fish-Cares-Wireframe%3Fnode-id%3D341-56776%26t%3DHdzoDnMwK2tRyWPQ-1" allowfullscreen></iframe>
+
+                            </div>
+
+                            <div class="col-12 col-6-lg">
+                                <h5>Design - Wireframe:</h5>
+                                <p>
+                                After completing the Brainstorm information architecture, before developing the website. We need to design a Style Guide for the company, which includes designing the logo, colors, fonts, buttons, wireframes, and mockups.
+                                </p>
+                            </div>
+                        
+                        </div>
+                        <div class='process col-12 grid'>
+                                <div class='col-12 col-6-lg'>
+                                    <h5>Development - hand code:</h5>
+                                    <p>
+                                    In the programming, I will be taught in
+                                    the classroom used to switch images
+                                    JavaScript integration into my home page
+                                    banner images so that they can be
+                                    constantly replaced, and in the future the
+                                    image needs to be replaced in fact do not
+                                    need to change the HTML and CSS and JS
+                                    parts, just need to re-upload the image
+                                    can be.<br><br>
+
+
+                                    I was also learning and experimenting
+                                    with different JavaScript libraries, and
+                                    eventually I chose AOS .js for my webpage
+                                    scrolling animation because I only needed
+                                    to have something to display when
+                                    scrolling to the content Compared to
+                                    ScrollMagic, it s much more suitable for a
+                                    normal website.<br><br>
+
+
+                                    I also used Bootstrap for the slideshow
+                                    part of the website, which saves me the
+                                    time of making a responsive website. I
+                                    also used Fancybox to show the zoom
+                                    function, and Typed.js to make the
+                                    typewriter function for the headline,
+                                    which makes my homepage more playful
+                                    and cute. <br><br>
+
+
+                                    Finally, I used Leaflet.js to create a map, so
+                                    that even if Whac-A-Mole Website Gameto add a new
+                                    location in the future, it can be marked on
+                                    the map, which is very flexible and
+                                    convenient.<br><br>
+                                    </p>
+                                </div>
+                            
+                            <div id="lottie-animation2" class='col-12 col-6-lg'></div>
+
+
+                        </div>
+
+                    </div>
+            </div>
+
+        </div>
+        <h2 class='other'>Other Project</h2>
+        <div class='ontherProject container grid' data-aos="fade-up">
+           
+                <div class='project col-12 col-6-sm col-6-md col-4-lg'>
+                    <div class='projectImg'><img src='./img/project/project2.png'  alt="Lumina online shopping Website"></div>
+                    <div class='projectInfo'>
+                        <h4>Lumina online shopping Website</h4>
+                        <a href='project2.php' class='viewBtn btn2'> View  <i class="fa-solid fa-arrow-right"></i> </a>
+                    </div>
+                </div>
+
+                <div class='project col-12 col-6-sm col-6-md col-4-lg'>
+                    <div class='projectImg'><img src='./img/project/project3.png' alt="Whac-A-Mole Website Game"></div>
+                    <div class='projectInfo'>
+                        <h4>Whac-A-Mole Website Game</h4>
+                        <a href='project3.php' class='viewBtn btn2'> View  <i class="fa-solid fa-arrow-right"></i> </a>
+                    </div>
+                </div>
+
+                <div class='project col-12 col-6-sm col-6-md col-4-lg'>
+                    <div class='projectImg'><img src='./img/project/project4.png' alt="October shopping app design"></div>
+                    <div class='projectInfo'>
+                        <h4>October shopping app design</h4>
+                        <a href='project4.php' class='viewBtn btn2'> View  <i class="fa-solid fa-arrow-right"></i> </a>
+                    </div>
+                </div>
+        </div>
+    </div>
+
+    </main>
+    <div class='content-wrap'></div>
+    <!----------------- footer ------------------>
+    <?php require_once("partials/footer.php") ?>
+    <!------------------------------------------->
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script src='./js/app.js'></script>
+    <script>
+        var animation = lottie.loadAnimation({
+        container: document.getElementById('lottie-animation'), // 容器 ID
+        renderer: 'svg', // 渲染器类型，可选 'svg' 或 'canvas'
+        loop: true, // 是否循环播放
+        autoplay: true, // 是否自动播放
+        path: 'https://lottie.host/50b088fe-b6b6-47b4-88f6-5d532aea5086/2a3vEVbGKX.json' // Lottie 文件的路径
+    });
+
+    var animation2 = lottie.loadAnimation({
+            container: document.getElementById('lottie-animation2'), // 容器 ID
+            renderer: 'svg', // 渲染器类型，可选 'svg' 或 'canvas'
+            loop: true, // 是否循环播放
+            autoplay: true, // 是否自动播放
+            path: 'https://lottie.host/62ecd630-5863-4f22-94ba-e80968fa0bb1/zCWiMOR3fs.json' // Lottie 文件的路径
+        });
+    </script>
+
+</body>
+</html>
